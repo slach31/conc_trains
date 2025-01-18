d2beab4d29465304921b294b1a2e20605502afa8
s23lachg <soufiane.lachguer@imt-atlantique.net>
2025-01-19 00:56:46 +0100

    new repo is created

---
+++ train/src/train/Train.java
@@ -0,0 +1,79 @@
+package train;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Représentation d'un train. Un train est caractérisé par deux valeurs :
+ * <ol>
+ *   <li>
+ *     Son nom pour l'affichage.
+ *   </li>
+ *   <li>
+ *     La position qu'il occupe dans le circuit (un élément avec une direction) : classe {@link Position}.
+ *   </li>
+ * </ol>
+ * 
+ * @author Fabien Dagnat <fabien.dagnat@imt-atlantique.fr>
+ * @author Mayte segarra <mt.segarra@imt-atlantique.fr>
+ * Test if the first element of a train is a station
+ * @author Philippe Tanguy <philippe.tanguy@imt-atlantique.fr>
+ * @version 0.3
+ */
+public class Train implements Runnable{
+	private final String name;
+	private final Position pos;
+
+	public Train(String name, Position p) throws BadPositionForTrainException {
+		if (name == null || p == null)
+			throw new NullPointerException();
+
+		// A train should be first be in a station
+		if (!(p.getPos() instanceof Station))
+			throw new BadPositionForTrainException(name);
+
+		this.name = name;
+		this.pos = p.clone();
+	}
+	
+	public void move(Element element) throws InterruptedException {
+		this.pos.setElement(element);
+		this.toString();
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder result = new StringBuilder("Train[");
+		result.append(this.name);
+		result.append("]");
+		result.append(" is on ");
+		result.append(this.pos);
+		return result.toString();
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		Element eltDummy = this.pos.getPos();
+		Railway r = eltDummy.getRailway();
+		Element[] eltsList = r.getElementsList();
+		
+		while (true) {
+			for (Element elt : eltsList) {
+				try {
+					move(elt);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				try {
+					Thread.sleep(10000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+}
